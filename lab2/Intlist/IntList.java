6e698fd48878527dd1c3f777e6d46f2084ea2b18
QI-Zzz <85391260+QI-Zzz@users.noreply.github.com>
2022-08-04 23:01:44 +0200

    Add files via upload

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,17 +81,33 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if(A == null){
+            return B;
+        }
+        if(A.rest == null){
+            A.rest = B;
+        }
+        else{
+            A.rest = dcatenate(A.rest , B);
+        }
+        return A;
     }
-
+    
     /**
      * Returns a list consisting of the elements of A followed by the
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+
+        IntList C = new IntList();
+        if(A == null){
+            return B;
+        }
+
+        else{
+            C = new IntList(A.first, catenate(A.rest, B));
+        }
+        return C;
     }
 
 
